165fcecf2eb43c1d33bbf46ec6adadb3059f92e7
YF <yf@liudeMacBook-Air.local>
2018-03-15 18:05:11 -0700

    Initial Commit

---
+++ one template/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  one template
+//
+//  Created by YF on 15/03/2018.
+//  Copyright © 2018 YF. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
